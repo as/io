6e653d50e75b8ff4d6c9df48e4bcb123fb177144
as <as.utf8@gmail.com>
2017-09-12 16:34:18 -0700

    fix spaz

--- spaz/spaz.go
+++ spaz/spaz.go
@@ -6,39 +6,19 @@ import (
 	"time"
 )
 
-type SpazReader struct {
-	buf []byte
-	rs  *rand.Rand
+type Reader struct{
+	ur io.Reader
+	rs *rand.Rand
 }
 
-// NewSpazReader creates a strange
-func NewReader(buf []byte) io.Reader {
-	return &SpazReader{buf, rand.New(rand.NewSource(time.Now().Unix()))}
+func NewReader(r io.Reader) *Reader{
+	return &Reader{ur: r, rs: rand.New(rand.NewSource(time.Now().Unix()))}
+}
+func (r *Reader) intn(n int)int{
+	return int(r.rs.Int31n(int32(n)))
 }
 
-// Read reads a random number of bytes into p. The number
-// of bytes read is a randomly-selected n between 0 and remainder
-// of bytes in the underlying buffer or the length of p.
-//
-func (sr *SpazReader) Read(p []byte) (n int, err error) {
-	k := len(sr.buf)
-	l := len(p)
-	m := min(k, l)
-
-	if k == 0 {
-		return 0, io.EOF
-	}
-
-	r := int(sr.rs.Int31n(int32(m)))
-	n = copy(p, sr.buf[:r])
-	sr.buf = sr.buf[n:]
-
-	return n, nil
-}
-
-func min(a, b int) int{
-	if a < b {
-		return a
-	}
-	return b
+func (r *Reader) Read(p []byte) (n int, err error) {
+	rn := r.intn(len(p))
+	return r.ur.Read(p[:rn])
 }
